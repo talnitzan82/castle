28030504e07da4e7f459a66d19351188c8d47c1f
talnitzan82@gmail.com <Talnitz1>
2017-03-20 19:19:17 +0200

    --

--- creative.php
+++ creative.php
@@ -1,12 +1,31 @@
 <?php
-//require_once (__DIR__."/utils/uuid.php");
+require_once (__DIR__."/utils/uuid.php");
 //require_once (__DIR__."/utils/utilFuncs.php");
 //require_once (__DIR__."/server/db.php");
-
+$uuid = UUID::v4();
 $ad_image = $_GET['ad'];
 $click_url = $_GET['click'];
 $height = $_GET['height'];
 $width = $_GET['width'];
-include "audit.js";
+$height = str_replace("'","",$height);
+$width = str_replace("'","",$width);
+$isAudit = isAudit($width,$height);
+
+$uAgent = $_SERVER['HTTP_USER_AGENT'];
+//TODO: split user agent
+
+//insert to DB
+
+if ($isAudit) {
+    include "audit.js";
+} else {
+    include "no_audit.js";
+}
+
+function isAudit($width,$height) {
+    if (!is_numeric($width) || !is_numeric($height)) {
+        return true;
+    };
+}
 ?>
     
---
+++ no_audit.js
@@ -0,0 +1,4 @@
+console.log('No Audit');
+var videoElement = '<a href="<?php echo $click_url ?>" target="_blank" style="height:<?php echo $height ?>px;width:<?php echo $width ?>px"><img src="<?php echo $ad_image?>" alt="banner" height="<?php echo $height ?>" width="<?php echo $width ?>"></a>';
+// var videoElement = '<img height="250" width="300" src="<?php echo $ad_image?>">';
+$('#main_script').parent().append(videoElement);

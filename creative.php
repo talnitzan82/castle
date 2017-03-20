f01fff1f543bca6f273f1339b0160ca1e5d4ad4f
Tal <talnitzan82@gmail.com>
2017-03-20 09:49:36 +0200

    first creative

---
+++ creative.php
@@ -0,0 +1,12 @@
+<?php
+//require_once (__DIR__."/utils/uuid.php");
+//require_once (__DIR__."/utils/utilFuncs.php");
+//require_once (__DIR__."/server/db.php");
+
+$ad_image = $_GET['ad'];
+$click_url = $_GET['click'];
+$height = $_GET['height'];
+$width = $_GET['width'];
+include "audit.js";
+?>
+    
